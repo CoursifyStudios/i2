51bca06b0b6243e1331f6d5fce406a1b9b803929
Brandon H <brandon@coursify.studio>
2023-05-29 23:06:14 -0700

    Changed favicon

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <div className="max-w-screen-xl flex mx-auto w-full">
+      <div>
+        <h1 className="text-3xl font-extrabold">ABOUT i2</h1>
+      </div>
+    </div>
+  );
+}
