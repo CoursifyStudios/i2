e0b7a43d7e43a3152d23966f3a1f5248fd4bee4d
brandnholl <brandnlholl@gmail.com>
2023-06-20 20:50:55 -0700

    Maybe this works?
    Co-authored-by: Lukas <quick007@users.noreply.github.com>

--- payload/collections/media.ts
+++ payload/collections/media.ts
@@ -4,16 +4,21 @@ import path from 'path';
 export const Media: CollectionConfig = {
   slug: 'media',
   upload: {
-    staticDir: 'https://cdn.coursify.studio'
+    staticDir: 'https://cdn.coursify.studio'   , 
+    adminThumbnail: ({ doc }) =>
+    `https://cdn.coursify.studio/i2-payload/${doc.filename}`,
   },
+  
   access: {
     read: () => true,
   },
   fields: [
+              
     {
       name: 'alt',
       type: 'text',
       required: true,
     },
+
   ]
 }
